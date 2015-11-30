eb2a6c3468c1e307be029016000611af25a9d99a
Tom Gillen <tom.gillen@graffica.co.uk>
2015-11-30 12:22:30 +0000

    Installed GitVersion task

--- MediaBrowser.Plugins.Anime/Properties/AssemblyInfo.cs
+++ MediaBrowser.Plugins.Anime/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1d0dddf7-1877-4473-8d7b-03f7dac1e559")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
