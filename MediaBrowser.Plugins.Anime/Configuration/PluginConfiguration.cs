25c8456144058b1d55548592800631c0ee3dc423
Matt07211 <matt07211@matt07211.xyz>
2019-01-25 17:18:04 +1100

    Revert Deletion of some code
    I missed this bit of code when reverting my changes, currently it will
    fail to build without it.

--- MediaBrowser.Plugins.Anime/Configuration/PluginConfiguration.cs
+++ MediaBrowser.Plugins.Anime/Configuration/PluginConfiguration.cs
@@ -34,6 +34,8 @@ namespace MediaBrowser.Plugins.Anime.Configuration
         public int MaxGenres { get; set; }
         public bool AddAnimeGenre { get; set; }
         public bool UseAnidbOrderingWithSeasons { get; set; }
+        public string MyAnimeList_API_Name { get; set; }
+        public string MyAnimeList_API_Pw { get; set; }
         public int AniDB_wait_time { get; set; }
         public PluginConfiguration()
         {
