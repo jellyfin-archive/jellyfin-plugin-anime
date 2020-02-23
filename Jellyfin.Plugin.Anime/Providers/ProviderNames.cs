802f1b0b9c21430b18e51d82973c4dcf9652e3c6
dkanada <dkanada@users.noreply.github.com>
2020-02-24 00:38:11 +0900

    remove proxer for now

--- Jellyfin.Plugin.Anime/Providers/ProviderNames.cs
+++ Jellyfin.Plugin.Anime/Providers/ProviderNames.cs
@@ -5,6 +5,5 @@
         public const string AniDb = "AniDB";
         public const string AniList = "AniList";
         public const string AniSearch = "AniSearch";
-        public const string Proxer = "Proxer";
     }
 }
